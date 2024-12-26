5d3afb041c24a7fe9a2343b0b1ac44854b5edcc7
Prasannakoduri <120080385+Prasannakodui@users.noreply.github.com>
2024-12-26 19:08:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.VITE_BASE_URL,
+  base: "https://admin.unimakler.com/",
   plugins: [
     react({
       fastRefresh: false
